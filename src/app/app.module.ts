e1324045873d4d8b54e5ecdd880d11aa8072ed46
S00190873 <S00190873@mail.itsligo.ie>
2020-11-17 16:33:26 +0000

    Bootstrap Lab Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/book.ts
+++
@@ -1,3 +0,0 @@
-export interface iBook {
-    title: String
-}
